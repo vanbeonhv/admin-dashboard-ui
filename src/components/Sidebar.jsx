cad46b6946236a1867bfde923858e5564d06205e
Phantom Lancer <nguyenvannhv26@gmail.com>
2022-09-19 19:39:25 +0700

    commit before merge

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import { SiShopware } from 'react-icons/si';
-import { MdOutlineCancel} from 'react-icons/md';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import React from "react";
+import { Link } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-import { links } from '../data/dummy';
+import { links } from "../data/dummy";
 
 const Sidebar = () => {
   const activeMenu = true;
 
   return (
-    <div className='ml-3 h-screen bg-slate-300'>
-      {activeMenu && (<React.Fragment>
-        <div className='flex justify-start items-center'>
-          <links to='/' onclick={() => {}} className='items-center flex m-4 ml-3'>
-            <SiShopware className='text-xl'/>
-            <span className='pl-3'>Shoppy</span>
-          </links>
-          </div> 
-          </React.Fragment>)}
-      </div>
-  )
-}
+    <div className="ml-3 h-screen bg-slate-300">
+      {activeMenu && (
+        <React.Fragment>
+          <div className="flex justify-start items-center">
+            <Link
+              to="/"
+              onClick={() => {}}
+              className="items-center flex m-4 ml-3"
+            >
+              <SiShopware className="text-xl" />
+              <span className="pl-3">Shoppy</span>
+            </Link>
+          </div>
+        </React.Fragment>
+      )}
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
